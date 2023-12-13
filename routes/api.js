64892b02aba532f06d40054b76ac67c0183ea2ac
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-12-13 18:31:29 +0800

    add: error messages and validation

--- routes/api.js
+++ routes/api.js
@@ -13,6 +13,22 @@ module.exports = function (app) {
 
       // console.log('translate', translator.translate(locale, text))
 
+      if(text === '') {
+        return res.json({ error: 'No text to translate' })
+      }
+
+      if(locale !== 'american-to-british' && locale !== 'british-to-american') {
+        return res.json({ error: 'Invalid value for locale field' })
+      }
+
+
+      if(!locale || !text) {
+        return res.json({ error: 'Required field(s) missing' })
+      }
+
+
+  
+
       const translation = translator.translate(locale, text)
       
       res.json({
