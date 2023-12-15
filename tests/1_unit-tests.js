9dd3bd7aa140c8a9d83fe773586dd1da6cc72eca
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-12-15 18:52:49 +0800

    add: british to american translation

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -111,7 +111,7 @@ suite('Unit Tests', () => {
             done();
         })
 
-        test("No Mr. Bond, I expect you to die.", (done) => {
+        test("Translate No Mr. Bond, I expect you to die. to British English", (done) => {
             let phrase = "No Mr. Bond, I expect you to die."
 
             const result = translator.translate(locale, phrase)
@@ -125,7 +125,7 @@ suite('Unit Tests', () => {
             done()
         })
 
-        test("Dr. Grosh will see you now.", (done) => {
+        test("Translate Dr. Grosh will see you now. to British English", (done) => {
             let phrase = "Dr. Grosh will see you now."
 
             const result = translator.translate(locale, phrase)
@@ -156,5 +156,140 @@ suite('Unit Tests', () => {
 
     suite('Translation from british to american', () => {
 
+        let locale = 'british-to-american'
+
+        test("Translate We watched the footie match for a while. to American English", (done) => {
+            let phrase = "We watched the footie match for a while."
+
+            const result = translator.translate(locale, phrase)
+            const res = removeHtml(result)
+
+            assert.notEqual(res, '')
+            assert.notEqual(res, undefined)
+            assert.notEqual(res, 'Everything looks good to me!')
+            assert.equal(res, 'We watched the soccer match for a while.')
+
+            done();
+        })
+
+        test("Translate Paracetamol takes up to an hour to work. to American English", (done) => {
+            let phrase = "Paracetamol takes up to an hour to work."
+
+            const result = translator.translate(locale, phrase)
+            const res = removeHtml(result)
+
+            assert.notEqual(res, '')
+            assert.notEqual(res, undefined)
+            assert.notEqual(res, 'Everything looks good to me!')
+            assert.equal(res, 'acetaminophen takes up to an hour to work.')
+
+            done()
+        })
+
+        test("Translate First, caramelise the onions. to American English", (done) => {
+            let phrase = "First, caramelise the onions."
+
+            const result = translator.translate(locale, phrase)
+            const res = removeHtml(result)
+
+            assert.notEqual(res, '')
+            assert.notEqual(res, undefined)
+            assert.notEqual(res, 'Everything looks good to me!')
+            assert.equal(res, 'First, caramelize the onions.')
+
+            done()
+        })
+
+        test("Translate I spent the bank holiday at the funfair. to American English", (done) => {
+            let phrase = "I spent the bank holiday at the funfair."
+
+            const result = translator.translate(locale, phrase)
+            const res = removeHtml(result)
+
+            assert.notEqual(res, '')
+            assert.notEqual(res, undefined)
+            assert.notEqual(res, 'Everything looks good to me!')
+            assert.equal(res, 'I spent the public holiday at the carnival.')
+
+            done()
+        })
+        
+        test("Translate I had a bicky then went to the chippy. to American English", (done) => {
+            let phrase = "I had a bicky then went to the chippy."
+            const result = translator.translate(locale, phrase)
+            const res = removeHtml(result)
+
+            assert.notEqual(res, '')
+            assert.notEqual(res, undefined)
+            assert.notEqual(res, 'Everything looks good to me!')
+            assert.equal(res, 'I had a cookie then went to the fish-and-chip shop.')
+
+            done()
+        })
+
+        test("Translate I've just got bits and bobs in my bum bag. to American English", (done) => {
+            let phrase = "I've just got bits and bobs in my bum bag."
+            const result = translator.translate(locale, phrase)
+            const res = removeHtml(result)
+
+            assert.notEqual(res, '')
+            assert.notEqual(res, undefined)
+            assert.notEqual(res, 'Everything looks good to me!')
+            assert.equal(res, "I've just got odds and ends in my fanny pack.")
+
+            done()
+        })
+
+        test("Translate The car boot sale at Boxted Airfield was called off. to American English" , (done) => {
+            let phrase = "The car boot sale at Boxted Airfield was called off."
+            const result = translator.translate(locale, phrase)
+            const res = removeHtml(result)
+
+            assert.notEqual(res, '')
+            assert.notEqual(res, undefined)
+            assert.notEqual(res, 'Everything looks good to me!')
+            assert.equal(res, "The swap meet at boxted airfield was called off.")
+
+            done()
+        })
+
+        test("Translate Have you met Mrs Kalyani? to American English", (done) => {
+            let phrase = "Have you met Mrs Kalyani?"
+            const result = translator.translate(locale, phrase)
+            const res = removeHtml(result)
+
+            assert.notEqual(res, '')
+            assert.notEqual(res, undefined)
+            assert.notEqual(res, 'Everything looks good to me!')
+            assert.equal(res, "Have you met Mrs. Kalyani?")
+
+            done()
+        })
+
+        test("Translate Prof Joyner of King's College, London. to American English", (done) => {
+            let phrase = "Prof Joyner of King's College, London."
+            const result = translator.translate(locale, phrase)
+            const res = removeHtml(result)
+
+            assert.notEqual(res, '')
+            assert.notEqual(res, undefined)
+            assert.notEqual(res, 'Everything looks good to me!')
+            assert.equal(res, "Prof. Joyner of king's college, london.")
+
+            done()
+        })
+
+        test("Translate Tea time is usually around 4 or 4.30. to American English", (done) => {
+            let phrase = "Tea time is usually around 4 or 4.30."
+            const result = translator.translate(locale, phrase)
+            const res = removeHtml(result)
+
+            assert.notEqual(res, '')
+            assert.notEqual(res, undefined)
+            assert.notEqual(res, 'Everything looks good to me!')
+            assert.equal(res, "Tea time is usually around 4 or 4:30.")
+
+            done()
+        })
     })
 });
